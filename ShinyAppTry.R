06fcf899bb88d5e91d70a0232e4d0e3df94d82f3
Faizan Khalid Mohsin <faizankms8@hotmail.com>
2021-04-30 18:54:58 -0400

    Initial Commit

---
+++ ShinyAppTry.R
@@ -0,0 +1,20 @@
+
+
+rsconnect::setAccountInfo(name='cubestatistics',
+                          token='8B49DEBB2E0D75FDD1456A8D73434B4F',
+                          secret='<SECRET>')
+
+rsconnect::setAccountInfo(name='cubestatistics', token='8B49DEBB2E0D75FDD1456A8D73434B4F', secret='dldgO6OQKEGrt8nBBtulyeEGWoH6t3NxrLbyO0Ju')
+
+
+library(rsconnect)
+rsconnect::deployApp('path/to/your/app')
+library(shiny)
+
+
+
+
+
+
+runApp()
+
